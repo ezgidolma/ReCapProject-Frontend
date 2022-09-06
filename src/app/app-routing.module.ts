d1aaab5d626323d9d49b4fda46a12bca29adc2cf
ezgidolma <88343326+ezgidolma@users.noreply.github.com>
2022-09-06 21:47:17 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarImageComponent } from './components/car-image/car-images.component';
 import { CarComponent } from './components/car/car.component';
-import { CardetailComponent } from './components/cardetail/cardetail.component';
+import { CarDetailComponent } from './components/cardetail/cardetail.component';
+
 
 const routes: Routes = [
-  {path:"",pathMatch:"full",component:CarComponent},
-  {path:"cars",component:CarComponent},
-  {path:"cars/brand/:brandId",component:CarComponent},
-  {path:"cars/color/:colorId",component:CarComponent},
-  {path:"carsdetails/detail/:carId",component:CardetailComponent},
-  {path:"carImage/:carId",component:CarImageComponent}
+  {path:"", pathMatch:"full",component:CarDetailComponent},
+  {path:"cars/cardetails/brand/:brandId", component:CarDetailComponent},
+  {path:"cars/cardetails/color/:colorId", component:CarDetailComponent},
+  {path:"cars/carimages/getbycarid:carId", component:CarImageComponent}
 ];
 
 @NgModule({
