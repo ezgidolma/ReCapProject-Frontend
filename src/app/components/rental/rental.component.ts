11b45cd318ab0c14788ef76739a6f84d94023bd9
ezgidolma <88343326+ezgidolma@users.noreply.github.com>
2022-10-08 19:35:31 +0300

    .

--- src/app/components/rental/rental.component.ts
+++ src/app/components/rental/rental.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { CarDetailDto } from 'src/app/models/Car/carDetailDto';
 import { Rental } from 'src/app/models/Rental/rental';
 import { RentalDetails } from 'src/app/models/Rental/rentaldetails';
+import { CustomerService } from 'src/app/services/customerservice/customer.service';
 import { RentalService } from 'src/app/services/rentalservice/rental.service';
 
 @Component({
@@ -22,9 +23,7 @@ export class RentalComponent implements OnInit {
   message:string|null;
   minDate:string|null
   maxDate:string|null
-  
   car:CarDetailDto;
-
   firstDateSelected:boolean= false; // rentDate seçili değilse, returnDate aktif olmayacak.
   state:number = 1;
   customerId:number
@@ -38,5 +37,14 @@ export class RentalComponent implements OnInit {
   ngOnInit(): void {
     
   }
+
+  getCarRentalDetails() {
+    this.rentalService.getCarRentalDetails().subscribe((response) => {
+      this.rentalDetails = response.data;
+    });
+  }
   
+  getCustomerDetails() {
+    CustomerService
+  }
 }
--- src/app/services/customerservice/customer.service.ts
+++ src/app/services/customerservice/customer.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customer } from 'src/app/models/Customer/customer';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { ResponseModel } from 'src/app/models/responseModel';
 
 
 @Injectable({
@@ -10,10 +11,25 @@ import { ListResponseModel } from 'src/app/models/listResponseModel';
 })
 export class CustomerService {
 
-  apiUrl="https://localhost:7266/api/customers/getcustomerdetails";
+  apiUrl="https://localhost:7266/api/";
   constructor(private httpclient:HttpClient) { }
 
-  getCustomers() : Observable<ListResponseModel<Customer>>{
-    return this.httpclient.get<ListResponseModel<Customer>>(this.apiUrl);
+  getCustomers():Observable<ListResponseModel<Customer>>{
+    return this.httpclient.get<ListResponseModel<Customer>>(
+      this.apiUrl +'Customers/GetAll'
+      );
+
+  }
+  getCustomerByUserId(userId:number) : Observable<ListResponseModel<Customer>>{
+    let newPath = this.apiUrl + "Customers/getbyuserid?userId=" + userId;
+    return this.httpclient.get<ListResponseModel<Customer>>(newPath);
+  }
+
+  addCustomer(customer:Customer):Observable<ResponseModel>{
+    return this.httpclient.post<ResponseModel>(this.apiUrl + "Customers/Add",customer)
+  }
+
+  updateCustomer(customer:Customer):Observable<ResponseModel>{
+    return this.httpclient.post<ResponseModel>(this.apiUrl + "Customers/Update",customer)
   }
 }
