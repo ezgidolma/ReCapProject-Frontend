91acae643e129ac095b4cad917aaef955d53b9ed
ezgidolma <88343326+ezgidolma@users.noreply.github.com>
2022-10-09 15:10:53 +0300

    .

--- src/app/services/brandservice/brand.service.ts
+++ src/app/services/brandservice/brand.service.ts
@@ -3,18 +3,38 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Brand } from 'src/app/models/Brand/brand';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { ResponseModel } from 'src/app/models/responseModel';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
-  apiUrl="https://localhost:7266/api/brands/getall";
+  urrentBrand:Brand;
+  apiUrl="https://localhost:7266/api/";
   constructor(private httpclient:HttpClient) { }
 
-  getBrands() : Observable<ListResponseModel<Brand>>{
-    return this.httpclient.get<ListResponseModel<Brand>>(this.apiUrl);
-  }
+  getBrands():Observable<ListResponseModel<Brand>> {
+    return this.httpclient.get<ListResponseModel<Brand>>(this.apiUrl+'brands/getall');
+    };
 
+    getCarsByBrandId(brandId:number):Observable<ListResponseModel<Brand>> {
+      let newPath=this.apiUrl+ "brands/getbyid?id="+brandId;
+      return this.httpclient.get<ListResponseModel<Brand>>(newPath);
+      }
+      
+      addBrand(brand: Brand):Observable<ResponseModel>{
+        return this.httpclient.post<ResponseModel>(this.apiUrl+"brands/add", brand)
+      }
+
+      deleteBrand(brand:Brand): Observable<ResponseModel>{
+        let newPath=this.apiUrl+ "brands/update";
+        return this.httpclient.post<ResponseModel>(newPath, brand);
+      }
+
+      updateBrand(brand:Brand): Observable<ResponseModel>{
+        let newPath=this.apiUrl+"brands/update";
+        return this.httpclient.post<ResponseModel>(newPath, brand);
+      }
 
 }
